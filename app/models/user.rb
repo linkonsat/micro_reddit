41396e46fbe492ea76f84b385f9d8ef1b3e3dfd8
MacDaniel Maisel <macdanielrmaisel@gmail.com>
2022-01-15 21:01:15 -0800

    Added user validations.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+    validates :name, length: { minimum: 2 }
+    validates :name, length: { maximum: 15 }
+    validates :name, uniqueness: true
     has_many :comment
     has_many :post
 end
